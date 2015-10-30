fb46f68040fbffaaab55b67f1c267739e7164f8a
ChipMorris10 <goldcomac@mac.com>
2015-10-30 11:59:01 -0600

    tested and passed all routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,12 +2,51 @@ var express = require('express');
 var router = express.Router();
 var Student = require('../models/student.js');
 
+// POST ONE student
+router.post('/students', function(req, res, next) {
+  new Student ({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    age: req.body.age,
+    married: req.body.married
+  })
+  .saveQ()
+    .then(function(result){res.json(result)})
+    .catch(function(err){res.send(err)})
+    .done();
+});
 
+// GET ONE student
+router.get('/student/:id', function(req, res, next){
+  Student.findByIdQ(req.params.id)
+    .then(function(result){res.json(result)})
+    .catch(function(err){res.send(err)})
+    .done();
+});
 
+// GET ALL students
+router.get('/students', function(req, res, next){
+  Student.findQ()
+    .then(function(result){res.json(result)})
+    .catch(function(err){res.send(err)})
+    .done();
+});
 
+// UPDATE ONE student
+router.put('/student/:id', function(req, res, next) {
+  Student.findByIdAndUpdateQ(req.params.id, req.body)
+    .then(function(result){res.json(result)})
+    .catch(function(err){res.send(err)})
+    .done();
+});
 
-
-
+// DELETE ONE student
+router.delete('/student/:id', function(req, res, next){
+  Student.findByIdAndRemoveQ(req.params.id)
+    .then(function(result){res.json(result)})
+    .catch(function(err){res.send(err)})
+    .done();
+});
 
 
 
